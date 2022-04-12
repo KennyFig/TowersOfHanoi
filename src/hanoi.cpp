c63b9ee4fbc60964b192d014d0437f0f83678b75
Kenneth P Figuerado III <kfiguerado18@gmail.com>
2022-04-11 20:37:53 -0400

    Added clarifying comments

--- src/hanoi.cpp
+++ src/hanoi.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 using namespace std;
-static int NUM_OF_RODS = 3;
+static int NUM_OF_RODS = 3; //Static number of rods
 void towerOfHanoi(int n, int fromRod, int toRod) {
-    if (n <= 0 || fromRod < 0 || toRod < 0) return;
+    if (n <= 0 || fromRod < 0 || toRod < 0) return; //Exit the function if there are no more disks or if the number assigned to the rod is invalid
     if(n == 1) {
-        cout << "Move top disk from rod " << fromRod << " to rod " << toRod << endl;
+        cout << "Move top disk from rod " << fromRod << " to rod " << toRod << endl; // Moves top disk
     } else {
-        int otherRod = NUM_OF_RODS - fromRod - toRod;
-        towerOfHanoi(n-1, fromRod,  otherRod);
-        cout << "Move top disk from rod " << fromRod << " to rod " << toRod << endl;
-        towerOfHanoi(n-1, otherRod, toRod);
+        int otherRod = NUM_OF_RODS - fromRod - toRod; //Calculate the other rod's number
+        towerOfHanoi(n-1, fromRod,  otherRod); //Move top disk from fromRod to otherRod
+        cout << "Move top disk from rod " << fromRod << " to rod " << toRod << endl; //print out moving the disk
+        towerOfHanoi(n-1, otherRod, toRod); //Move top disk from otherRod to toRod
     }
 }
 int main() {
     towerOfHanoi(3, 0, 2);
-    return 0;
+    return 0; 
 }
